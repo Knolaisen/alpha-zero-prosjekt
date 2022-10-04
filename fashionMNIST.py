0d9ebceca12e0f6ca6cfda3a22c7be1a03a8e80d
Kent Roger Vistven <kenta123456789@hotmail.com>
2022-10-04 17:10:07 +0200

    la til convolutional nettverk

--- fashionMNIST.py
+++ fashionMNIST.py
@@ -43,21 +43,31 @@ print(f"Using {device} device")
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, padding=1),
+            nn.BatchNorm2d(10),
             nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 10)
+            #nn.MaxPool2d(kernel_size=2, stride=2)
         )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=28, kernel_size=3),
+            nn.BatchNorm2d(28),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc1 = nn.Linear(in_features=4732, out_features=10)
+        self.drop = nn.Dropout2d(0.25)
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.flatten(out)
+        out = self.fc1(out)
+        out = self.drop(out)
+
+        
+        return out
 
 model = NeuralNetwork().to(device)
 print(model)
@@ -100,7 +110,7 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs1 = 20
+epochs1 = 5
 epochs2 = 5
 
 for t in range(epochs1):
@@ -134,6 +144,6 @@ classes = [
 model.eval()
 x, y = test_data[0][0], test_data[0][1]
 with torch.no_grad():
-    pred = model(x)
+    pred = model(x.unsqueze(0))
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
