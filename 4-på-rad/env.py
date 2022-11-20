95e3d5c25eec08ca64f8aa0d72145e19a6ff249a
Fredrik Randsborg Bølstad <fredrrbo@stud.ntnu.no>
2022-11-20 20:33:37 +0100

    Definert 4-på-rad environment

--- 4-på-rad/env.py
+++ 4-på-rad/env.py
@@ -2,24 +2,94 @@ import numpy as np
 
 
 class ConnectFour():
-    def __init__(self, rows:int = 6, columns:int = 7):
-        self.rows = rows
-        self.columns = 7
-        self.board = np.zeros((rows, columns))
+    def __init__(self, shape: tuple = (6, 7)):
+        self.shape = shape
+        self.board = np.zeros(shape)
 
-    def get_reward(self):
-        pass
+    def get_board(self) -> np.array:
+        return self.board
 
-    def get_board(self):
-        print(self.board)
-    
-    def get_available_moves(self):
-        pass
-    
-    def move(self):
-        pass
-    
+    def _get_next_open_row(self, column: int) -> int:
+        # Check for free square from bottom-up
+        for row_idx in range(self.shape[0]-1, -1, -1):
+
+            # Return the row immediately if free
+            if self.board[row_idx][column] == 0:
+                return row_idx
+
+        # Return None if no free squares in given column
+        return None
+
+    def is_legal_move(self, column: int) -> bool:
+        if column < 0 or column >= self.shape[1]:
+            raise Exception(
+                f"Column {column} is greater than board with shape {self.shape}")
+
+        if self._get_next_open_row(column) is None:
+            return False
+        else:
+            return True
+
+    def move(self, column: int, player: int) -> None:
+        if player is None or column is None:
+            raise Exception(
+                "Missing required arguments (requires column and player)")
+
+        # Validate player
+        if not (player == -1 or player == 1):
+            raise Exception("Player must be -1 or 1")
+
+        # Check if legal move
+        row = self._get_next_open_row(column)
+        if row is None:
+            raise Exception(f"Illegal move. Column {column} is full")
+
+        # Do move
+        self.board[row][column] = player
+
+    def get_available_moves(self) -> list[int]:
+        return [column for column in range(self.shape[1]) if self._get_next_open_row(column) is not None]
+
     def is_win_state(self):
-        pass
-    
-    
+        # Test rows
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1] - 3):
+                value = sum(self.board[i][j:j + 4])
+                if abs(value) == 4:
+                    winner = 1 if value > 0 else -1
+                    return (True, winner)
+
+        # Test columns on transpose array
+        reversed_board = [list(i) for i in zip(*self.board)]
+        for i in range(self.shape[1]):
+            for j in range(self.shape[0] - 3):
+                value = sum(reversed_board[i][j:j + 4])
+                if abs(value) == 4:
+                    winner = 1 if value > 0 else -1
+                    return (True, winner)
+
+        # Test diagonal
+        for i in range(self.shape[0] - 3):
+            for j in range(self.shape[1] - 3):
+                value = 0
+                for k in range(4):
+                    value += self.board[i + k][j + k]
+                    if abs(value) == 4:
+                        winner = 1 if value > 0 else -1
+                        return (True, winner)
+
+        reversed_board = np.fliplr(self.board)
+        # Test reverse diagonal
+        for i in range(self.shape[0] - 3):
+            for j in range(self.shape[1] - 3):
+                value = 0
+                for k in range(4):
+                    value += reversed_board[i + k][j + k]
+                    if abs(value) == 4:
+                        winner = 1 if value > 0 else -1
+                        return (True, winner)
+
+        return (False, 0)
+
+    def is_game_over(self) -> bool:
+        return len(self.get_available_moves()) == 0 or self.is_win_state()[0]
