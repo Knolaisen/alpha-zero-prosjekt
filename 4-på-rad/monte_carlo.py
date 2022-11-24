565cf766854ecde43fbffb4d293bec7f65b5f503
PerIvarFrib <pif-99@hotmail.com>
2022-11-24 16:41:39 +0100

    Rafinert neuralt netverk & bynt på integrert MCTS

--- 4-på-rad/monte_carlo.py
+++ 4-på-rad/monte_carlo.py
@@ -1,40 +1,54 @@
+import neural_network as NN
+import env
 
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+print(f"Using {device} device")
 
-# main function for the Monte Carlo Tree Search
-def monte_carlo_tree_search(root):
-	
-	while resources_left(time, computational power):
-		leaf = traverse(root)
-		simulation_result = rollout(leaf)
-		backpropagate(leaf, simulation_result)
-		
-	return best_child(root)
+class MonteCarlo():
 
-# function for node traversal
-def traverse(node):
-	while fully_expanded(node):
-		node = best_uct(node)
-		
-	# in case no children are present / node is terminal
-	return pick_unvisited(node.children) or node
+	def __init__(self) -> None:
+		self.board = env.ConnectFour.get_board()	
 
-# function for the result of the simulation
-def rollout(node):
-	while non_terminal(node):
-		node = rollout_policy(node)
-	return result(node)
+	# main function for the Monte Carlo Tree Search
+	def monte_carlo_tree_search(root):
 
-# function for randomly selecting a child node
-def rollout_policy(node):
-	return pick_random(node.children)
+#		policy, value = NN.forward(self.board)
+#		for possible_action in policy:
+			
 
-# function for backpropagation
-def backpropagate(node, result):
-	if is_root(node) return
-	node.stats = update_stats(node, result)
-	backpropagate(node.parent)
+		while resources_left(time, computational power):
+			leaf = traverse(root)
+			simulation_result = rollout(leaf)
+			backpropagate(leaf, simulation_result)
+			
+		return best_child(root)
 
-# function for selecting the best child
-# node with highest number of visits
-def best_child(node):
-	pick child with highest number of visits
+	# function for node traversal
+	def traverse(node):
+		while fully_expanded(node):
+			node = best_uct(node)
+			
+		# in case no children are present / node is terminal
+		return pick_unvisited(node.children) or node
+
+	# function for the result of the simulation
+	def rollout(node):
+		while non_terminal(node):
+			node = rollout_policy(node)
+		return result(node)
+
+	# function for randomly selecting a child node
+	def rollout_policy(node):
+		return pick_random(node.children)
+
+	# function for backpropagation
+	def backpropagate(node, result):
+		if is_root(node) return
+		node.stats = update_stats(node, result)
+		backpropagate(node.parent)
+
+	# function for selecting the best child
+	# node with highest number of visits
+	def best_child(node):
+		pick child with highest number of visits
--- 4-på-rad/neural_network.py
+++ 4-på-rad/neural_network.py
@@ -7,9 +7,8 @@ from env import ConnectFour
 # URL: https://github.com/Knolaisen/alpha-zero-prosjekt/blob/Per-Ivar/examples/fashionMNIST.py
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda"
 print(f"Using {device} device")
-
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -19,19 +18,19 @@ class NeuralNetwork(nn.Module):
             nn.Conv2d(in_channels=1, out_channels=6, kernel_size=[1, 7]).to(device),
             # nn.BatchNorm1d(6),
             nn.ReLU(),
-            #        nn.MaxPool2d(kernel_size=2, stride=2)
+            # nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
         self.layer_vertical = nn.Sequential(
             nn.Conv2d(in_channels=6, out_channels=7, kernel_size=[6, 1]).to(device),
             # nn.BatchNorm1d(7),
             nn.ReLU(),
-            #        nn.MaxPool2d(kernel_size=2, stride=2)
+            # nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
         self.layer_4x4 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=4).to(device),
-            nn.BatchNorm1d(3),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=4, device=device),
+            nn.BatchNorm1d(3, device=device),
             nn.ReLU(),
             # nn.MaxPool2d(kernel_size=2, stride=2)
         )
@@ -41,9 +40,9 @@ class NeuralNetwork(nn.Module):
         self.policy = nn.Sequential(
             nn.Linear(in_features=384, out_features=64, device=device),
             nn.ReLU(),
-            nn.Linear(in_features=64, out_features=32),
+            nn.Linear(in_features=64, out_features=32, device=device),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=7),
+            nn.Linear(in_features=32, out_features=7, device=device),
             nn.Softmax(dim=0),
             
         )
@@ -51,43 +50,37 @@ class NeuralNetwork(nn.Module):
         self.value = nn.Sequential(
             nn.Linear(in_features=384, out_features=64, device=device),
             nn.ReLU(),
-            nn.Linear(in_features=64, out_features=32),
+            nn.Linear(in_features=64, out_features=32, device=device),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=1),
+            nn.Linear(in_features=32, out_features=1, device=device),
             nn.Sigmoid()
         )
 
-        # self.fc1 = nn.Linear(in_features=1600, out_features=64)
-        # self.drop = nn.Dropout2d(0.25)
-
     def forward(self, x):
+        out = torch.from_numpy(x).to(device=device, dtype=torch.float)
+        shape = 1, *game.shape
+        out = out.view(shape)
+
         # out = self.layer_horizontal(x)
         # out = self.layer_vertical(out)
-        out = self.layer_4x4(x)
+        out = self.layer_4x4(out)
         out = self.flatten(out)
 
-        policy = self.policy(out)
-        value = self.value(out)
-
-        # out = self.flatten(out)
-        # out = self.fc1(out)
-    #    out = self.drop(out)
+        policy = self.policy(out).detach().cpu().numpy()
+        value = self.value(out).detach().cpu().numpy()
 
         return policy, value
 
 
-model = NeuralNetwork().to(device)
-print(model)
+model = NeuralNetwork().to(device=device)
+#print(model)
 # print(sum(param.numel() for param in model.parameters()))
 
 game = ConnectFour()
-input_tensor = torch.from_numpy(game.get_board()).to(device)
-input_tensor = input_tensor.type(torch.FloatTensor)
-# input_tensor = input_tensor.resize_(6, 7, 1)
-input_tensor = input_tensor.view(1, 6, 7)
-
-print(input_tensor.shape)
+state = game.get_board()
 
 nn = NeuralNetwork()
-forward = nn.forward(input_tensor)
-print(forward)
+policy, value = nn.forward(state)
+#print(forward)
+
+print(f'\n\nPolicy: {policy}\n Value:{value}')
