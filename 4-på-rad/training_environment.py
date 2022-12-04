d7fffa8df4bfdabe052c41f1fe68a20e197d2d97
PerIvarFrib <pif-99@hotmail.com>
2022-12-04 10:15:27 +0100

    tweaked for iterative training

--- 4-på-rad/training_environment.py
+++ 4-på-rad/training_environment.py
@@ -2,64 +2,95 @@ from neural_network import NeuralNetwork, device
 from torch import nn
 import torch
 import pickle
-
-#TODO Importere modellen som allerede har blitt trent så man slipper å starte fra scratch hver gang
+import numpy as np
 
 # Array storing all training data: board state, policy target and value target respectively 
-filename_training = 'training_data'
-try:
-	infile_training = open(filename_training, 'rb')
-	training_data = pickle.load(infile_training)
-	infile_training.close()
-except (OSError, IOError) as e:
-	print('No training data available')
+def fetch_training_data(filename='training_data') -> object:
+    try:
+        infile = open(filename, 'rb')
+        data = pickle.load(infile)
+        infile.close()
+        return data
+    except (OSError, IOError) as e:
+        print('No training data available')
 
-connect_4_brain = NeuralNetwork().to(device=device)
-torch.autograd.set_detect_anomaly(True)
+def load_network(name='./connect_4_brain.pth', device=device, debug=False) -> object:
+    PATH = name
+    network = NeuralNetwork().to(device=device)
+    try:
+        network.load_state_dict(torch.load(PATH))
+        if debug:
+            print(f'Loading network from path:{PATH}')
+        return network
+    except (OSError, IOError) as e:
+        print('Could not find pre-existing network. Returning randomly initialized network')
+        return network
 
-criterion_policy = nn.CrossEntropyLoss()
-criterion_value = nn.MSELoss()
+#torch.autograd.set_detect_anomaly(True)
 
-learning_rate = 0.001
-optimizer = torch.optim.Adam(params=connect_4_brain.parameters(), lr=learning_rate)
+def train(network, training_data, epochs=1, debug=False) -> None:
 
-number_of_possible_moves = 7
+    criterion_policy = nn.CrossEntropyLoss()
+    criterion_value = nn.MSELoss()
 
-epochs = 1
-for epoch in range(epochs):
+    #learning_rate = 8e-7
+    learning_rate = 1e-2
+    weight_decay = 1e-2
+    optimizer = torch.optim.Adam(params=network.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-    running_loss_policy = 0.0
-    running_loss_value = 0.0
-    for i, data in enumerate(training_data, 0):
-        # getting inputs
-        board_state, policy_target, value_target = data
+    number_of_possible_moves = 7
 
-        # converting to tensor
-        policy_target = torch.from_numpy(policy_target).to(device=device, dtype=float)
-        value_target = torch.from_numpy(value_target).to(device=device, dtype=torch.float32)
+    for epoch in range(epochs):
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+        running_loss_policy = 0.0
+        running_loss_value = 0.0
+        training_data_shape = np.shape(training_data)
 
-        # forward + backward + optimize
-        policy_output, value_output = connect_4_brain.forward(board_state, tensor=True)
-        loss_policy = criterion_policy(policy_output, policy_target)
-        loss_policy.backward(retain_graph=True)
-        loss_value = criterion_value(value_output, value_target)
-        loss_value.backward()
-        optimizer.step()
+        for i, data in enumerate(training_data, 0):
+            # getting inputs
+            board_state, policy_target, value_target = data
 
-        # print statistics for policy
-        running_loss_policy += loss_policy.item()
-        running_loss_value += loss_value.item()
-        stat_batch = 50
-        if i % stat_batch == stat_batch-1: # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss_policy: {running_loss_policy / stat_batch:.3f}')
-            print(f'[{epoch + 1}, {i + 1:5d}] loss_value: {running_loss_value / stat_batch:.3f}')
-            running_loss_policy = 0.0
-            running_loss_value = 0.0
+            # converting to tensor
+            policy_target = torch.from_numpy(policy_target).to(device=device, dtype=float)
+            value_target = torch.from_numpy(value_target).to(device=device, dtype=torch.float32)
+      
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-print('Finished Training')
+            # forward + backward + optimize
+            policy_output, value_output = network.forward(board_state, tensor=True)
+            if debug:
+                print(f'{i}:\n policy target: {policy_target}\npolicy output: {policy_output}')
+                print(f'value target: {value_target}\nvalue output: {value_output}')
+            loss_policy = criterion_policy(policy_output, policy_target)
+            loss_policy.backward(retain_graph=True)
+            loss_value = criterion_value(value_output, value_target)
+            loss_value.backward()
+            if loss_policy:
+                optimizer.step()
 
-PATH = './connect_4_brain.pth'
-torch.save(connect_4_brain.state_dict(), PATH)
+            # print statistics for policy
+            running_loss_policy += loss_policy.item()
+            running_loss_value += loss_value.item()
+            #stat_batch = 5
+            #if i % stat_batch == stat_batch-1: # print every 2000 mini-batches
+            roof = training_data_shape[0]-1
+            if i == roof:
+                #print(f'[{epoch + 1}, {i + 1:5d}] loss_policy: {running_loss_policy / stat_batch:.3f}, loss_value: {running_loss_value / stat_batch:.3f}')
+                print(f'[{epoch + 1}, {i + 1:5d}] loss_policy: {running_loss_policy / (roof+1):.3f}, loss_value: {running_loss_value / (roof+1):.3f}')
+                running_loss_policy = 0.0
+                running_loss_value = 0.0
+
+    #print('Finished Training')
+
+def save_network(network, name='./connect_4_brain.pth', debug=False) -> None:
+    PATH = name
+    torch.save(network.state_dict(), PATH)
+    if debug:
+        print(f'Saving neural network to path: {PATH}')
+
+# --- OPTIMIZATION TRICKS --- #
+# Disable bias for convolutions directly followed by a batch norm
+# This works since the biases are canceled through batch norm anyways, hence it changes nothing and speeds it up
+# use: nn.Conv2d(..., bias=False, ....)
+# note: batchnorm needs to normalize on the same dimension as the conv-biases are in
