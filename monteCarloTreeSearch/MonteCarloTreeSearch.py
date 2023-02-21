ce1f99a5e3cd0e5c7a3d766f96acb500c88f7b5c
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-21 19:02:20 +0100

    #1 Add methods selection,  expansion, monte_carlo_tree_search, upper_condidence_bound, backpropergation

---
+++ monteCarloTreeSearch/MonteCarloTreeSearch.py
@@ -0,0 +1,30 @@
+## Dependencies
+import numpy as np
+
+
+def monte_carlo_tree_search():
+    pass
+
+def selection():
+    pass
+
+def expansion():
+    """
+    In this process, a new child node is added to the tree to that node which was optimally reached during the selection process.
+    """
+    pass
+
+def simulation():
+    """
+    In this process, a simulation is performed by choosing moves or strategies until a result or predefined state is achieved.
+    """
+    pass
+
+def backpropergation():
+    """
+    After determining the value of the newly added node, the remaining tree must be updated. So, the backpropagation process is performed, where it backpropagates from the new node to the root node. During the process, the number of simulation stored in each node is incremented. Also, if the new node’s simulation results in a win, then the number of wins is also incremented.
+    """
+    pass
+
+def upperCondidenceBound():
+    pass
