a0120fb5e0f8f6711531311903c769a730818cdd
SaiYue <74411800+SaiYueWasTaken@users.noreply.github.com>
2023-02-28 18:48:47 +0100

    #2 Update folder structure

---
+++ src/monteCarloTreeSearch/MonteCarloTreeSearch.py
@@ -0,0 +1,38 @@
+## Dependencies
+import math
+import numpy as np
+import TreeNode
+
+
+def monte_carlo_tree_search():
+    pass
+
+def selection():
+    pass
+
+def expansion():
+    """
+    In this process, a new child node is added to the tree to that node which was optimally reached during the selection process.
+    """
+    pass
+
+def simulation():
+    """
+    In this process, a simulation is performed by choosing moves or strategies until a result or predefined state is achieved.
+    """
+    pass
+
+def backpropergation():
+    """
+    After determining the value of the newly added node, the remaining tree must be updated. So, the backpropagation process is performed, where it backpropagates from the new node to the root node. During the process, the number of simulation stored in each node is incremented. Also, if the new node’s simulation results in a win, then the number of wins is also incremented.
+    """
+    pass
+
+def upper_condidence_bound(empiricalMean: float, visitationOfParentNode: int, visitationOfChildNode: int ) -> float:
+    constant = math.sqrt(2)
+    exploredness = math.sqrt(math.log(visitationOfParentNode)/visitationOfChildNode)
+    return empiricalMean + constant * exploredness
+ 
+def ucb(node: TreeNode ):
+    
+    pass
