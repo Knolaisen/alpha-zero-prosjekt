9b7bc88958b6e842a48f5ca586d8b29e79312563
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-02-28 18:59:39 +0100

    #1 Create state class

--- monteCarloTreeSearch/state.py
+++ monteCarloTreeSearch/state.py
@@ -1,10 +1,33 @@
 from abc import ABC, abstractmethod
 
-class state_handler(ABC=metaABC):
+class state_handler(ABC):
     
 	@abstractmethod
-	def isFinished():
+	def is_finished(self) -> bool:
+		"""
+		When the game is done is_finished will be true.
+		"""
 		pass
-	
+
+	@abstractmethod
+	def get_winner(self) -> int:
+		"""
+		Whom won this game
+		"""
+		pass
+
+	@abstractmethod
+	def get_legal_actions(self) -> list:
+		"""
+		Get a list of legal action at current game state
+		"""
+		pass
+
+	@abstractmethod
+	def move(self, action):
+		"""
+		Do the move in the game
+		"""
+		pass
 	
 
