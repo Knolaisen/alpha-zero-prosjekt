ad0a4f2206e5306c116f6142f04c8e5d64427a09
1rideee <einri-o@online.no>
2023-03-15 17:24:39 +0100

    #2 improve documentation of chessstatehandler

--- src/monteCarloTreeSearch/ChessStateHandler.py
+++ src/monteCarloTreeSearch/ChessStateHandler.py
@@ -3,32 +3,32 @@ import chess
 
 class ChessState(state_handler):
 	def __init__(self):
-		# Initialize the chess board here
-		# You can use a library like python-chess to represent the board and handle moves
+		"""Initialize the chess board """
 		self.Board = chess.Board()
 		
 	def is_finished(self) -> bool:
-		# Check if the game is finished (e.g. checkmate, stalemate, draw)
-		# Return True if the game is finished, False otherwise
+		"""Check if the game is finished (e.g. checkmate, stalemate, draw)
+		Return True if the game is finished, False otherwise"""
 		if (self.Board.is_variant_draw or self.Board.is_variant_loss or self.Board.is_variant_win):
 			return True
 		else:
 			return False 
 
 	def get_winner(self) -> int:
-		# Determine the winner of the game (-1 for black, 0 for draw, 1 for white)
-		# Return the winner as an integer
+		"""Determine the winner of the game (-1 for black, 0 for draw, 1 for white)
+		Return the winner as an integer"""
 		if (self.Board.is_variant_draw):
 			return 0
 		elif(self.Board.is_variant_loss & self.Board.turn == "WHITE"):
 			return -1
 		elif(self.Board.is_variant_loss & self.Board.turn == "BLACK"): 
 			return 1
+		else:
+			assert "Game not finished!!"
 
 	def get_legal_actions(self) -> list:
-		# Get a list of legal moves for the current state of the game
-		# You can use a library like python-chess to generate legal moves
-		# Return the legal moves as a list
+		"""Generates a list of legal moves for the current state of the game
+		Return the legal moves as a list"""
 		moves_list = []
 		for i in self.Board.legal_moves:
 			moves_list.append(i)
@@ -36,12 +36,10 @@ class ChessState(state_handler):
 		return moves_list
 
 	def move(self, action) -> None:
-		# Execute a move on the chess board
-		# You can use a library like python-chess to execute the move
+		"""Takes in a move and performs it on the board"""
+
 		self.Board.push(action)
 
 	def get_state(self):
-		# Get the current state of the chess board
-		# You can use a library like python-chess to represent the board state
-		# Return the board state
+		"""Gives the current state of the board, uset to visualize board"""
 		return self.Board
