858928705612950c6f646beee89721b03f0a4a8e
1rideee <einri-o@online.no>
2023-03-15 17:18:31 +0100

    #2 update chess example

--- misc/ChessEx.py
+++ misc/ChessEx.py
@@ -8,6 +8,7 @@ board = chess.Board()
 
 board.legal_moves  
 
+# making a list of legal moves
 
 print(board.legal_moves)
 moves_list = []
@@ -16,20 +17,21 @@ for i in board.legal_moves:
     print(i)
     moves_list.append(i)
 
-print(board)
-board.push(moves_list[0])
-print(board)
-
-# board.push_san("e4")
-# board.push_san("e5")
-# board.push_san("Qh5")
-# board.push_san("Nc6")
-# board.push_san("Bc4")
-# board.push_san("Nf6")
-# board.push_san("Qxf7")
-# print(board.is_checkmate())
+# how to add move from legal moves list
 # print(board)
-# print(board.legal_moves)
+# board.push(moves_list[0])
+# print(board)
+
+board.push_san("e4")
+board.push_san("e5")
+board.push_san("Qh5")
+board.push_san("Nc6")
+board.push_san("Bc4")
+board.push_san("Nf6")
+board.push_san("Qxf7")
+print(board.is_checkmate())
+print(board)
+print(board.legal_moves)
 
 
 
