dde4c6de7b25038cee6b1e1e522434d6c39e6248
1rideee <einri-o@online.no>
2023-04-25 21:10:30 +0200

    stuff

--- src/visualize.py
+++ src/visualize.py
@@ -12,7 +12,10 @@ def visualize(board):
     # Define colors
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    RED = 	(255, 0, 0)
+    GREEN = (0, 255, 0)
     # Main game loop
+    selected_square = None
     running = True
     while running:
         for event in pygame.event.get():
@@ -25,6 +28,13 @@ def visualize(board):
                 file = int(x / (WINDOW_SIZE[0] / 8))
                 rank = int((WINDOW_SIZE[1] - y) / (WINDOW_SIZE[1] / 8))
                 square = chess.square(file, rank)
+
+                #implement such that second 
+                if (selected_square==square):
+                    pass
+                
+                # Set the selected square to the clicked square
+                selected_square = square
                 # Print the square that was clicked
                 print(f"You clicked on square {chess.square_name(square)}")
         # Draw the board on the screen
@@ -38,6 +48,9 @@ def visualize(board):
                     square_color = WHITE
                 else:
                     square_color = BLACK
+                # If the square is selected, highlight it in red
+                if selected_square == square:
+                    square_color = RED
                 # Draw the square on the screen
                 pygame.draw.rect(screen, square_color, pygame.Rect(file * 100, (7 - rank) * 100, 100, 100))
                 # Draw the piece on the square, if there is one
@@ -68,7 +81,7 @@ def visualize(board):
                             piece_img = pygame.image.load("src\Imgs\\b_queen.png")
                         else:
                             piece_img = pygame.image.load("src\Imgs\\b_king.png")
-                    screen.blit(piece_img, pygame.Rect(file * 100 + 40, (7 - rank) * 100 +10, 10, 0))
+                    screen.blit(piece_img, pygame.Rect(file * 100 + 40, (7 - rank) * 100 +10, 100, 100))
         pygame.display.flip()
         # Pause for a short period to prevent the window from closing immediately
         pygame.time.wait(10)
